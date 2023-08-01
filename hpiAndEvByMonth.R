0ca05d0b943f6ede14bff2776a3feb5c2eb8ce76
bliu <brianliu03@gmail.com>
2023-07-31 22:35:55 -0700

    Clean data

--- hpiAndEvByMonth.R
+++ hpiAndEvByMonth.R
@@ -8,28 +8,22 @@ hpi_data <- as_tibble(data.frame(
     read.csv("hpi.csv", header = TRUE, sep = ",")
 ))
 
-# clean data
-evn_data <- evn_data %>%
-    mutate(
-        file_date = as.Date(file_date, format = "%m/%d/%Y"),
-        month = format(file_date, "%m"),
-        year = format(file_date, "%Y")
-    ) %>%
-    group_by(year, month) %>%
-    summarise(
-        evn_count = n()
-    )
+# change to date format
+evn_data$Date <- as.Date(evn_data$File.Date, format = "%m/%d/%Y")
+hpi_data$Date <- as.Date(hpi_data$date, format = "%Y-%m-%d")
 
+# hpi only goes to june 2023 so use eviction data up to that point
+# evn only starts in 1997-01-01 so use hpi data from that point
+evn_data <- evn_data %>%
+    filter(Date < "2023-06-01")
 hpi_data <- hpi_data %>%
-    mutate(
-        month = format(date, "%m"),
-        year = format(date, "%Y")
-    ) %>%
-    filter(
-        year >= 1997,
-        year <= 2018
-    ) %>%
-    group_by(year, month) %>%
-    summarise(
-        hpi = mean(hpi)
-    )
+    filter(Date >= "1997-01-01")
+
+# since hpi data is monthly, group eviction data by month and year
+# also change date format to just year and month
+evn_data <- evn_data %>%
+    group_by(year = format(Date, "%Y"), month = format(Date, "%m")) %>%
+    summarise(count = n())
+hpi_data <- hpi_data %>%
+    group_by(year = format(Date, "%Y"), month = format(Date, "%m")) %>%
+    summarise(hpi = mean(hpi))
