842711edb8971cad3a0c991ab8916e42d8464d26
bliu <brianliu03@gmail.com>
2023-08-09 22:37:24 -0700

    Copy error in py

--- evictions.py
+++ evictions.py
@@ -2,20 +2,21 @@ import pandas as pd
 
 # Read the CSV file and only keep the relevant columns
 evictions = pd.read_csv("sfevictionnotices.csv", usecols=["File Date", "Non Payment", "Breach", "Nuisance",
-                                                    "Illegal Use", "Failure to Sign Renewal",
-                                                    "Access Denial", "Unapproved Subtenant",
-                                                    "Owner Move In", "Demolition", "Capital Improvement",
-                                                    "Substantial Rehab", "Ellis Act WithDrawal",
-                                                    "Condo Conversion", "Roommate Same Unit",
-                                                    "Other Cause", "Late Payments",
-                                                    "Lead Remediation", "Development",
-                                                    "Good Samaritan Ends"])
+                                                          "Illegal Use", "Failure to Sign Renewal",
+                                                          "Access Denial", "Unapproved Subtenant",
+                                                          "Owner Move In", "Demolition", "Capital Improvement",
+                                                          "Substantial Rehab", "Ellis Act WithDrawal",
+                                                          "Condo Conversion", "Roommate Same Unit",
+                                                          "Other Cause", "Late Payments",
+                                                          "Lead Remediation", "Development",
+                                                          "Good Samaritan Ends"])
 
 # Reshape the DataFrame to long format
 evictions_long = pd.melt(evictions, id_vars=["File Date"], value_vars=["Non Payment", "Breach", "Nuisance",
                                                                        "Illegal Use", "Failure to Sign Renewal",
                                                                        "Access Denial", "Unapproved Subtenant",
-                                                                       "Owner Move In", "Demolition", "Capital Improvement",
+                                                                       "Owner Move In", "Demolition",
+                                                                       "Capital Improvement",
                                                                        "Substantial Rehab", "Ellis Act WithDrawal",
                                                                        "Condo Conversion", "Roommate Same Unit",
                                                                        "Other Cause", "Late Payments",
@@ -26,7 +27,7 @@ evictions_long = pd.melt(evictions, id_vars=["File Date"], value_vars=["Non Paym
 # Filter the rows where the condition is met
 filtered_evictions = evictions_long[evictions_long["Is True"]]
 
-# Drop the "Is True" column
+filtered_evictions = evictions_long[evictions_long["Is True"]].copy()
 filtered_evictions.drop("Is True", axis=1, inplace=True)
 
 # Display the resulting DataFrame
