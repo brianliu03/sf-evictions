1a320d7c6f1cf6743af4d68e5b582f2ff89e0451
Brian Liu <64610885+brianliu03@users.noreply.github.com>
2023-09-20 21:01:40 -0700

    prop counts over mean

--- rfiles/season.R
+++ rfiles/season.R
@@ -7,7 +7,7 @@ eviction_data <- read.csv("sfevictionnotices.csv", header = TRUE, sep = ",")
 eviction_data <- as_tibble(data.frame(eviction_data))
 
 eviction_data <- restructure(eviction_data)
-# eviction_data <- filter_no_2023(eviction_data)
+eviction_data <- filter_no_2023(eviction_data)
 eviction_data <- get_counts_by_month_year(eviction_data)
 
 # Assuming you have the eviction_counts data frame as described
@@ -84,3 +84,44 @@ ggplot(eviction_proportions, aes(x = factor(Year), y = Proportion, fill = as.fac
     axis.text.x = element_text(angle = 45, hjust = 1),  # Rotate x-axis labels for better readability
     legend.position = "none"  # Remove legend
   )
+
+
+
+# Calculate the total number of evictions for each month
+# Add mean eviction count for each month
+# Add median eviction count for each month
+total_evictions_by_month <- eviction_data %>%
+  group_by(Month, Month_Name) %>%
+  summarize(Total_Evictions = sum(Eviction_Count),
+            Mean_Evictions = mean(Eviction_Count),
+            Median_Evictions = median(Eviction_Count))
+
+# Calculate the proportion of counts for each month over expected
+# value and median value
+eviction_proportions <- eviction_data %>%
+  select(-Month_Name) %>%
+  left_join(total_evictions_by_month, by = "Month") %>%
+  mutate(Proportion_Mean = Eviction_Count / Mean_Evictions,
+         Proportion_Median = Eviction_Count / Median_Evictions)
+
+# Apply base-2 logarithm transformation to Proportion_Mean and Proportion_Median
+eviction_proportions_transformed <- eviction_proportions %>%
+  mutate(
+    Transformed_Proportion_Mean = log(Proportion_Mean, base = 2),
+    Transformed_Proportion_Median = log(Proportion_Median, base = 2)
+  )
+
+# Create a ggplot to visualize the proportions by month
+ggplot(eviction_proportions_transformed, aes(x = factor(Month_Name, levels = month.name), y = Transformed_Proportion_Mean, group = as.factor(Year), color = as.factor(Year))) +
+  geom_line(size = 1) +
+  geom_hline(yintercept = 0, size = 0.5, color = "black") +
+  labs(
+    title = "Proportion: Evictions Counts per Month over Mean Evictions by Month",
+    x = "Month",  # Remove x-axis label
+    y = "Proportion of Evictions (log_2)"
+  ) +
+  theme_minimal() +
+  theme(axis.text.x = element_blank()) +  # Remove x-axis text labels
+  scale_color_manual(values = my_palette, name = "Month", guide = "none") +
+  facet_wrap(~Year, ncol = 13)  # Create separate line plots for each year (13 columns in this example)
+
