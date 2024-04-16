577b3c6a749c92b611c8c77624345080490ba21c
Brian Liu <64610885+brianliu03@users.noreply.github.com>
2024-04-16 15:25:05 +0100

    eda python

--- evictions.py
+++ evictions.py
@@ -1,15 +1,18 @@
 import pandas as pd
 
 # Read the CSV file and only keep the relevant columns
-evictions = pd.read_csv("sfevictionnotices.csv", usecols=["File Date", "Non Payment", "Breach", "Nuisance",
-                                                          "Illegal Use", "Failure to Sign Renewal",
-                                                          "Access Denial", "Unapproved Subtenant",
-                                                          "Owner Move In", "Demolition", "Capital Improvement",
-                                                          "Substantial Rehab", "Ellis Act WithDrawal",
-                                                          "Condo Conversion", "Roommate Same Unit",
-                                                          "Other Cause", "Late Payments",
-                                                          "Lead Remediation", "Development",
-                                                          "Good Samaritan Ends"])
+evictions = pd.read_csv(
+    "sfevictionnotices.csv",
+    usecols=["File Date", "Non Payment", "Breach", "Nuisance",
+            "Illegal Use", "Failure to Sign Renewal",
+            "Access Denial", "Unapproved Subtenant",
+            "Owner Move In", "Demolition", "Capital Improvement",
+            "Substantial Rehab", "Ellis Act WithDrawal",
+            "Condo Conversion", "Roommate Same Unit",
+            "Other Cause", "Late Payments",
+            "Lead Remediation", "Development",
+            "Good Samaritan Ends"]
+)
 
 # Reshape the DataFrame to long format to put the eviction types in one column
 evictions = pd.melt(evictions, id_vars=["File Date"], value_vars=["Non Payment", "Breach", "Nuisance",
